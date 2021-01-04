c2baf4c2a4b02f9b55d473d4037824c92164ffaa
Jason Ittner <jasonittner@gmail.com>
2021-01-04 17:13:41 -0500

    Implement pages for user's top artists/tracks

---
+++ front-end/src/pages/TopArtists.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import styled from 'styled-components';
+import Loader from '../components/Loader';
+import { media } from '../style/media';
+import theme from '../style/theme';
+const { colors } = theme;
+
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 50px;
+    ${media.tablet`
+        display: block;
+        text-align: center;
+    `};
+    h2 {
+        margin: 0;
+    }
+`;
+const Ranges = styled.div`
+    display: flex;
+    margin-right: -11px;
+`;
+const RangeButton = styled.button`
+    background-color: transparent;
+    border-width: 0;
+    color: ${props => (props.isActive ? colors.white : colors.lightGrey)};
+    font-size: 20px;
+    font-weight: 500;
+    padding: 10px;
+    span {
+        padding-bottom: 2px;
+        border-bottom: 1px solid ${props => (props.isActive ? colors.green : `transparent`)};
+        line-height: 1.5;
+        white-space: nowrap;
+    };
+    &:focus {
+        outline: none;
+    };
+    &:hover {
+        color: #FFFFFF;
+    }
+`;
+const ArtistsContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-gap: 20px;
+    margin-top: 50px;
+`;
+const Artist = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+`;
+const ArtistArtwork = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 200px;
+  height: 200px;
+  img {
+    border-radius: 100%;
+    object-fit: cover;
+    width: 200px;
+    height: 200px;
+  }
+`;
+const ArtistName = styled.a`
+  margin: 30px 0;
+  color: #FFFFFF;
+  text-decoration: none;
+  &:hover {
+    color: #FFFFFF;
+  }
+`;
+
+class TopArtists extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            activeRange: 'long_term',
+            topArtists: {
+                'long_term': null,
+                'medium_term': null,
+                'short_term': null,
+            },
+            errorMessage: null,
+        };
+    }
+    getData(range) {
+        fetch(`http://localhost:5000/user/top-artists/${range}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',        
+            },
+            credentials: 'include'
+        })
+        .then(async response => {
+            const topArtists = await response.json();
+            if (!response.ok){
+                const error = (topArtists && topArtists.message) || response.status;
+                return Promise.reject(error);
+            }
+            const newState = Object.assign({}, this.state);
+            newState.topArtists[range] = topArtists;
+            this.setState(newState);
+        })
+        .catch(error => {
+            this.setState({errorMessage: error})
+        });
+    }
+    async changeRange(range) {
+        if (!this.state.topArtists[range]) {
+            this.getData(range);
+        }
+        this.setState({activeRange: range});
+    }
+    componentDidMount() {
+        this.getData('long_term');
+    }
+
+    render() {
+        const activeRange = this.state.activeRange;
+        const topArtists = this.state.topArtists[activeRange];
+
+        return (
+            <div>
+                <Container className="p-2">
+                    <Header>
+                        <h2>Top Artists</h2>
+                        <Ranges>
+                            <RangeButton
+                                isActive={activeRange === 'long_term'}
+                                onClick={() => this.changeRange('long_term')}>
+                                <span>All Time</span>
+                            </RangeButton>
+                            <RangeButton
+                                isActive={activeRange === 'medium_term'}
+                                onClick={() => this.changeRange('medium_term')}>
+                                <span>Last 6 Months</span>
+                            </RangeButton>
+                            <RangeButton
+                                isActive={activeRange === 'short_term'}
+                                onClick={() => this.changeRange('short_term')}>
+                                <span>Last 4 Weeks</span>
+                            </RangeButton>
+                        </Ranges>
+                    </Header>
+                    <ArtistsContainer>
+                        {topArtists ? (
+                            topArtists.data.map(({ id,  external_urls, images, name }, idx) => (
+                                <Artist key={idx}>
+                                    <ArtistArtwork>
+                                        {images.length && <img src={images[1].url} alt="Artist" />}
+                                    </ArtistArtwork>
+                                    <ArtistName href={external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                                        {name}
+                                    </ArtistName>
+                                </Artist>
+                            ))
+                        )
+                        :
+                        (
+                        <Loader />
+                        )
+                        }    
+                    </ArtistsContainer>
+                </Container>
+            </div>
+        );
+    } 
+}
+
+export default TopArtists;
---
+++ front-end/src/pages/TopTracks.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import styled from 'styled-components';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image';
+import Loader from '../components/Loader';
+import TrackObject from '../components/TrackObject';
+import { media } from '../style/media';
+import theme from '../style/theme';
+const { colors } = theme;
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 50px;
+    ${media.tablet`
+        display: block;
+        text-align: center;
+    `};
+    h2 {
+        margin: 0;
+    }
+`;
+const Ranges = styled.div`
+    display: flex;
+    margin-right: -11px;
+`;
+const RangeButton = styled.button`
+    background-color: transparent;
+    border-width: 0;
+    color: ${props => (props.isActive ? colors.white : colors.lightGrey)};
+    font-size: 20px;
+    font-weight: 500;
+    padding: 10px;
+    span {
+        padding-bottom: 2px;
+        border-bottom: 1px solid ${props => (props.isActive ? colors.green : `transparent`)};
+        line-height: 1.5;
+        white-space: nowrap;
+    };
+    &:focus {
+        outline: none;
+    };
+    &:hover {
+        color: #FFFFFF;
+    }
+`;
+const TracksContainer = styled.ul`
+    margin-top: 50px;
+    list-style-type: none;
+    padding-left: 0;
+`;
+
+class TopTracks extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            activeRange: 'long_term',
+            topTracks: {
+                'long_term': null,
+                'medium_term': null,
+                'short_term': null,
+            },
+            errorMessage: null,
+        };
+    }
+    getData(range) {
+        fetch(`http://localhost:5000/user/top-tracks/${range}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',        
+            },
+            credentials: 'include'
+        })
+        .then(async response => {
+            const topTracks = await response.json();
+            if (!response.ok){
+                const error = (topTracks && topTracks.message) || response.status;
+                return Promise.reject(error);
+            }
+            const newState = Object.assign({}, this.state);
+            newState.topTracks[range] = topTracks;
+            this.setState(newState);
+        })
+        .catch(error => {
+            this.setState({errorMessage: error})
+        });
+    }
+    async changeRange(range) {
+        if (!this.state.topTracks[range]) {
+            this.getData(range);
+        }
+        this.setState({activeRange: range});
+    }
+    componentDidMount() {
+        this.getData('long_term');
+    }
+    render() {
+        const activeRange = this.state.activeRange;
+        const topTracks = this.state.topTracks[activeRange];
+
+        return (
+            <div>
+                <Container className="p-2">
+                    <Header>
+                        <h2>Top Tracks</h2>
+                        <Ranges>
+                            <RangeButton
+                                isActive={activeRange === 'long_term'}
+                                onClick={() => this.changeRange('long_term')}>
+                                <span>All Time</span>
+                            </RangeButton>
+                            <RangeButton
+                                isActive={activeRange === 'medium_term'}
+                                onClick={() => this.changeRange('medium_term')}>
+                                <span>Last 6 Months</span>
+                            </RangeButton>
+                            <RangeButton
+                                isActive={activeRange === 'short_term'}
+                                onClick={() => this.changeRange('short_term')}>
+                                <span>Last 4 Weeks</span>
+                            </RangeButton>
+                        </Ranges>
+                    </Header>
+                    <TracksContainer>
+                        {topTracks ? (
+                            topTracks.data.map((track) => (
+                                <TrackObject track={track} />
+                            ))
+                        )
+                        :
+                        (<Loader />)  
+                        }
+                    </TracksContainer>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default TopTracks;
